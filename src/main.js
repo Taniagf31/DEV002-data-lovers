e3114a68d7731a17a5f7ae71ef060210ce7c242d
tania lizbeth garcia fierro <taniafe98@gmail.com>
2022-12-25 21:22:10 -0600

    se logra filtrar por director

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ console.log(example, data);
 
 const tarjetas = document.getElementById("tarjetas");
 data.films.forEach(element => {
-    const tarjeta = `
+  const tarjeta = `
       <div class="contenedor"> 
             <section class="imagen"><figure><img id="poster" src="${element.poster}"</figure></section>
             <section class="texto">
@@ -16,16 +16,39 @@ data.films.forEach(element => {
             </section>
       </div>
       `
-    tarjetas.innerHTML += tarjeta
+  tarjetas.innerHTML += tarjeta
 });
 
 if (todas) {
   //botón todas
-  document.getElementById("todas").addEventListener("click",function() {
+  document.getElementById("todas").addEventListener("click", function () {
+    const tarjetas = document.getElementById("tarjetas");
+    tarjetas.innerHTML = "";
+    console.log(tarjetas);
+    data.films.forEach(element => {
+      const tarjeta = `
+    <div class="contenedor"> 
+          <section class="imagen"><figure><img id="poster" src="${element.poster}"</figure></section>
+          <section class="texto">
+            <p id= "titulo"> ${element.title}</p>
+            <p id= "datos"> Director: ${element.director} <br> Productor: ${element.producer} <br> Lanzamiento: ${element.release_date} <br> Score: ${element.rt_score}</p>
+          </section>
+    </div>
+    `
+      tarjetas.innerHTML += tarjeta
+    });
+    console.log("Mostrar Todas");
+  });
+}
+//botón Hayao
+document.getElementById("Hayao Miyazaki").addEventListener("click", function (event) {
+  console.log(event.target.id);
+  //enlazar función filter de data.js
+  let hayao = filterBy(event.target.id, data); //enviar dos argumentos
+  console.log(hayao)
   const tarjetas = document.getElementById("tarjetas");
   tarjetas.innerHTML = "";
-  console.log (tarjetas);
-data.films.forEach(element => {
+  hayao.forEach(element => {
     const tarjeta = `
     <div class="contenedor"> 
           <section class="imagen"><figure><img id="poster" src="${element.poster}"</figure></section>
@@ -35,21 +58,136 @@ data.films.forEach(element => {
           </section>
     </div>
     `
-    tarjetas.innerHTML += tarjeta});
-    console.log ("Mostrar Todas");
+    tarjetas.innerHTML += tarjeta
   });
-} 
-//botón Hayao
-document.getElementById("Hayao Miyazaki").addEventListener("click",function(event) { 
-  console.log (event.target.id);
-  filterBy(event.target.id, data); //enviar dos argumentos
-  //enlazar función filter de data.js
+  let menu = document.getElementById("lista")
+  menu.style.display = "none"
 });
 
-document.getElementById("reciente").addEventListener("click",function() { 
-  console.log ("Muestra Más recientes");
-  //enlazar función filter de data.js
+//botón Isao
+document.getElementById("Isao Takahata").addEventListener("click", function (event) {
+  console.log(event.target.id);
+  
+  let isao = filterBy(event.target.id, data); //enviar dos argumentos
+  console.log(isao)
+  const tarjetas = document.getElementById("tarjetas");
+  tarjetas.innerHTML = "";
+  isao.forEach(element => {
+    const tarjeta = `
+    <div class="contenedor"> 
+          <section class="imagen"><figure><img id="poster" src="${element.poster}"</figure></section>
+          <section class="texto">
+            <p id= "titulo"> ${element.title}</p>
+            <p id= "datos"> Director: ${element.director} <br> Productor: ${element.producer} <br> Lanzamiento: ${element.release_date} <br> Score: ${element.rt_score}</p>
+          </section>
+    </div>
+    `
+    tarjetas.innerHTML += tarjeta
+  });
+  let menu = document.getElementById("lista")
+  menu.style.display = "none"
 });
+
+//botón Yoshifumi 
+document.getElementById("Yoshifumi Kondō").addEventListener("click", function (event) {
+  console.log(event.target.id);
+  
+  let kondo = filterBy(event.target.id, data); //enviar dos argumentos
+  console.log(kondo)
+  const tarjetas = document.getElementById("tarjetas");
+  tarjetas.innerHTML = "";
+  kondo.forEach(element => {
+    const tarjeta = `
+    <div class="contenedor"> 
+          <section class="imagen"><figure><img id="poster" src="${element.poster}"</figure></section>
+          <section class="texto">
+            <p id= "titulo"> ${element.title}</p>
+            <p id= "datos"> Director: ${element.director} <br> Productor: ${element.producer} <br> Lanzamiento: ${element.release_date} <br> Score: ${element.rt_score}</p>
+          </section>
+    </div>
+    `
+    tarjetas.innerHTML += tarjeta
+  });
+  let menu = document.getElementById("lista")
+  menu.style.display = "none"
+});
+
+//botón Hiroyuki Morita
+document.getElementById("Hiroyuki Morita").addEventListener("click", function (event) {
+  console.log(event.target.id);
+  
+  let morita = filterBy(event.target.id, data); //enviar dos argumentos
+  console.log(morita)
+  const tarjetas = document.getElementById("tarjetas");
+  tarjetas.innerHTML = "";
+  morita.forEach(element => {
+    const tarjeta = `
+    <div class="contenedor"> 
+          <section class="imagen"><figure><img id="poster" src="${element.poster}"</figure></section>
+          <section class="texto">
+            <p id= "titulo"> ${element.title}</p>
+            <p id= "datos"> Director: ${element.director} <br> Productor: ${element.producer} <br> Lanzamiento: ${element.release_date} <br> Score: ${element.rt_score}</p>
+          </section>
+    </div>
+    `
+    tarjetas.innerHTML += tarjeta
+  });
+  let menu = document.getElementById("lista")
+  menu.style.display = "none"
+});
+//botón Gorō Miyazaki
+document.getElementById("Gorō Miyazaki").addEventListener("click", function (event) {
+  console.log(event.target.id);
+  
+  let goro = filterBy(event.target.id, data); //enviar dos argumentos
+  console.log(goro)
+  const tarjetas = document.getElementById("tarjetas");
+  tarjetas.innerHTML = "";
+  goro.forEach(element => {
+    const tarjeta = `
+    <div class="contenedor"> 
+          <section class="imagen"><figure><img id="poster" src="${element.poster}"</figure></section>
+          <section class="texto">
+            <p id= "titulo"> ${element.title}</p>
+            <p id= "datos"> Director: ${element.director} <br> Productor: ${element.producer} <br> Lanzamiento: ${element.release_date} <br> Score: ${element.rt_score}</p>
+          </section>
+    </div>
+    `
+    tarjetas.innerHTML += tarjeta
+  });
+  let menu = document.getElementById("lista")
+  menu.style.display = "none"
+});
+
+//botón Hiromasa Yonebayashi
+document.getElementById("Hiromasa Yonebayashi").addEventListener("click", function (event) {
+  console.log(event.target.id);
+  
+  let hiromasa = filterBy(event.target.id, data); //enviar dos argumentos
+  console.log(hiromasa)
+  const tarjetas = document.getElementById("tarjetas");
+  tarjetas.innerHTML = "";
+  hiromasa.forEach(element => {
+    const tarjeta = `
+    <div class="contenedor"> 
+          <section class="imagen"><figure><img id="poster" src="${element.poster}"</figure></section>
+          <section class="texto">
+            <p id= "titulo"> ${element.title}</p>
+            <p id= "datos"> Director: ${element.director} <br> Productor: ${element.producer} <br> Lanzamiento: ${element.release_date} <br> Score: ${element.rt_score}</p>
+          </section>
+    </div>
+    `
+    tarjetas.innerHTML += tarjeta
+  });
+  let menu = document.getElementById("lista")
+  menu.style.display = "none"
+});
+
+
+//document.getElementById("reciente").addEventListener("click",function() {
+  //console.log ("Muestra Más recientes");
+  //enlazar función filter de data.js
+//});
 
 
 
